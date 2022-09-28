f9923538a87fc7d21110bfec2f34d4c0535fcb66
j.perrochaud <j.perrochaud@student.isartdigital.com>
2022-09-28 11:43:30 +0200

    deleted:    src/game.o 	deleted:    src/gui.d 	deleted:    src/gui.o 	modified:   src/main.c 	deleted:    src/main.d 	deleted:    src/main.o 	deleted:    tests/test_button.d 	deleted:    tests/test_button.o

--- src/gui.d
+++
@@ -1,2 +0,0 @@
-src/gui.o: src/gui.c ../playground/externals/include/GLFW/glfw3.h \
- ../playground/playground.h src/gui.h
--- tests/test_button.d
+++
@@ -1,3 +0,0 @@
-tests/test_button.o: tests/test_button.c \
- ../playground/externals/include/GLFW/glfw3.h ../playground/playground.h \
- tests/../src/gui.h
